5387e0e50dcd2b9105a6adf8e3989f7dc6898d2e
Petr Sushilin <95305151+petrSushilin@users.noreply.github.com>
2024-11-11 22:22:20 +0300

    initial commit

---
+++ src/main/java/ru/t1/school/open/project/JavaProjectT1Application.java
@@ -0,0 +1,13 @@
+package ru.t1.school.open.project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaProjectT1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaProjectT1Application.class, args);
+    }
+
+}
